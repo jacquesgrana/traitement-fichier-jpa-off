c55e5e4b32f1d861fe6f1057d8ceef3fb9c6b735
Jacques Granarolo <jac.granarolo.info@gmail.com>
2022-07-14 17:47:24 +0200

    init projet

---
+++ src/test/java/fr/diginamic/javaFS2022/traitement_fichier_jpa_off/AppTest.java
@@ -0,0 +1,20 @@
+package fr.diginamic.javaFS2022.traitement_fichier_jpa_off;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
